fe357a017428d8d2644854d2f2a2d381105a0ac3
Pau <Pau.correo@ejemplo.com>
2025-11-15 17:30:29 +0100

    1º versión

---
+++ src/componentes1/Componentes1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package componentes1;
+
+/**
+ *
+ * @author pauhd
+ */
+public class Componentes1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
